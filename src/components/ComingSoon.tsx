c94a9169d24255c87cd66a78f71411b6467aef4a
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2024-02-28 08:47:05 +0800

    change to video background instead of image

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -4,14 +4,23 @@ import './ComingSoon.scss';
 const ComingSoon: React.FC = () => {
   return (
     <div className="container">
-      <div className="logo-container">
-        <img className="logo" src="https://i.ibb.co/bPvqK6B/Careero-Logo-HD.png" alt="Logo" />
-        <div className="logo-text">
-          <h1>Coming Soon!</h1>
-          <p>Stay tuned for our exciting launch.</p>
+      <div className="video-background">
+        <iframe
+          title="Embedded Video"
+          src="https://streamable.com/e/krcg65?autoplay=1&nocontrols=1"
+          allowFullScreen
+          frameBorder="0"
+        />
+      </div>
+      <div className="content-container">
+        <div className="logo-container">
+          <img className="logo" src="https://i.ibb.co/bPvqK6B/Careero-Logo-HD.png" alt="Logo" />
+          <div className="logo-text">
+            <h1>Coming Soon!</h1>
+            <p>Stay tuned for our exciting launch.</p>
+          </div>
         </div>
       </div>
-      <img className="portrait" src="https://i.ibb.co/JqLwwzN/careero.jpg" alt="Portrait" />
     </div>
   );
 };
